838f3f644aac5c35f9f070fba7b6fc2372c881b4
Jakub Pajor <jay.pajor@gmail.com>
2019-02-22 14:05:25 +0100

    Post added

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from app import app, db, User
+from app import app, db, User, Post
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Post=Post)
