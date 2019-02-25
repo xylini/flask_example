6c38cca2d5906d37c63747ab0222eb6d8d3cbc68
Jakub Pajor <jay.pajor@gmail.com>
2019-02-25 20:33:32 +0100

    new models, new cnfg

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+import datetime
+
 from flask import Flask
+from flask_migrate import Migrate
 from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255))
 
     posts = db.relationship(
@@ -18,6 +23,8 @@ class User(db.Model):
         lazy='dynamic'
     )
 
+
+
     def __init__(self, username):
         self.username = username
 
@@ -25,12 +32,35 @@ class User(db.Model):
         return "<User '{}'>".format(self.username)
 
 
+tags = db.Table('post_tags',
+                db.Column('post_id', db.Integer(), db.ForeignKey('post.id')),
+                db.Column('tag_id', db.Integer(), db.ForeignKey('tag.id')))
+
 class Post(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255))
+    title = db.Column(db.String(255), nullable=False)
     text = db.Column(db.Text())
-    publish_date = db.Column(db.DateTime())
+    publish_date = db.Column(db.DateTime(), default=datetime.datetime.now())
+
+    # Many-one
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+
+    # One-many
+    comments = db.relationship(
+        'Comment',
+        backref='post',
+        lazy='dynamic'
+    )
+
+    # many - many
+    tags = db.relationship(
+        'Tag',
+        secondary=tags,
+        backref=db.backref('posts', lazy='dynamic')
+    )
+
+
 
     def __init__(self, title):
         self.title = title
@@ -38,6 +68,37 @@ class Post(db.Model):
     def __repr__(self):
         return "<Post '{}'>".format(self.title)
 
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime(), default=datetime.datetime.now())
+
+    # Many - One
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    # def __init__(self, title):
+    #     self.title = title
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+# a lower-level access to the database than the abstraction of
+# db.Model
+
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
 
 
 @app.route('/')
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from app import app, db, User, Post
+from app import app, db, User, Post, Comment, Tag, tags, migrate
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post)
+    return dict(app=app, db=db, User=User, Post=Post, Comment=Comment, Tag=Tag, tags=tags, migrate=migrate)
